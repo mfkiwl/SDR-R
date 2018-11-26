fc149e5f7f490071e0ad224c6de15b16d2c1bd42
C C <carrera1@umbc.edu>
2018-11-26 16:37:01 -0500

    Updated script to utilize all 4 cores
    Previously only used 2 cores because I made it in a VM.

--- fpga-src/uart_test/SynthImp.tcl
+++ fpga-src/uart_test/SynthImp.tcl
@@ -13,9 +13,9 @@
 open_project ./Cmod-A7-35T-GPIO.xpr
 update_compile_order -fileset sources_1
 reset_run synth_1
-launch_runs synth_1 -jobs 2
+launch_runs synth_1 -jobs 4
 wait_on_run synth_1
-launch_runs impl_1 -jobs 2
+launch_runs impl_1 -jobs 4
 wait_on_run impl_1
-launch_runs impl_1 -to_step write_bitstream -jobs 2
+launch_runs impl_1 -to_step write_bitstream -jobs 4
 wait_on_run impl_1
